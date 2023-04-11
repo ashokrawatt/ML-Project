c1da460346c8defeb698bc2994f7231e6f5b39f5
Ashok singh <rwtashok2000@gmail.com>
2023-04-11 08:41:49 +0530

    fix the error in data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,7 +11,8 @@ from src.components.data_transformation import DataTransformation
 
 ## Intitialize the Data Ingetion Configuration
 
-@dataclass
+@dataclass.
+
 class DataIngestionconfig:
     train_data_path:str=os.path.join('artifacts','train.csv')
     test_data_path:str=os.path.join('artifacts','test.csv')
